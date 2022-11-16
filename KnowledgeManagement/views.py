e7a8fb9c5393a9d0dfd7114d75f81cf5329cc5c7
venom-mancer <99116977+venom-mancer@users.noreply.github.com>
2022-11-16 17:50:12 +0330

    Update : 	modified:   KnowledgeManagement/views.py 	modified:   templates/_layout.html

--- KnowledgeManagement/views.py
+++ KnowledgeManagement/views.py
@@ -30,7 +30,9 @@ from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views import generic
-from django.db.models import Count , Q , F
+from django.db.models import Count
+from django.db.models import Q
+from django.db.models import F
 from KnowledgeManagement.notifications import km_notifications
 from .forms import PasswordChangingForm,FileModelForm,Informs_meetingForm, TblKnowledgeValueITForm,TblUserRewardsForm, indicator_KeyForm,TblChartAdvanceInfoForm, TblKnowledgeErteghaForm,TblQuestionRequestForm, TblJalaseForm, TblSurveyForm,send_to_evaluatorForm,FeedFile_JalaseForm,send_invite,rejectForm
 from django.http.response import JsonResponse
